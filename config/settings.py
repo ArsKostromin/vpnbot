de1383177f5d9d595ab72014242198db66efa245
SKYFALL <arsyakost@gmail.com>
2025-05-02 01:16:02 +0500

    q

--- config/settings.py
+++ config/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 import os
 
 
-ALLOWED_HOSTS = ["vpnbot.onrender.com", "localhost", "127.0.0.1", "backend", "backend:8000", 'vpnbot.onrender.com', '159.198.77.150']
+ALLOWED_HOSTS = ["vpnbot.onrender.com", "localhost", "127.0.0.1", "backend", "backend:8000", "159.198.77.222"]
 DEBUG = False if os.getenv("RENDER") else True
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
