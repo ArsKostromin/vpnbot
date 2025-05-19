1fc6badc864c5062a7750a7724ba3ae0cf39419c
SKYFALL <arsyakost@gmail.com>
2025-05-19 05:13:58 +0500

    q

--- proxy_logs/admin.py
+++ proxy_logs/admin.py
@@ -3,5 +3,5 @@ from .models import ProxyLog
 
 @admin.register(ProxyLog)
 class ProxyLogAdmin(admin.ModelAdmin):
-    list_display = ("user", "timestamp", "remote_ip", "domain", "status", "bytes_sent")
-    search_fields = ("user", "raw_log", "domain", "remote_ip")
+    list_display = ("user", "timestamp", "remote_ip", "domain")
+    search_fields = ("raw_log", "domain", "remote_ip", "user__email", "user__telegram_id")
