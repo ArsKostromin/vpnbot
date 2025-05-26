fe367f0b85cf486bb5096d8b1e7fcf9662f90dd8
ArsKostromin <arsyakost@gmail.com>
2025-05-27 03:43:43 +0500

    q

--- vpn_api/admin.py
+++ vpn_api/admin.py
@@ -2,12 +2,32 @@
 from django.contrib import admin
 from .models import SubscriptionPlan, Subscription
 
+
 @admin.register(SubscriptionPlan)
 class SubscriptionPlanAdmin(admin.ModelAdmin):
     list_display = ('vpn_type', 'duration', 'price', 'discount_percent', 'discount_price', 'discount_active')
     list_filter = ('vpn_type', 'duration', 'discount_active')
     search_fields = ('vpn_type',)
     readonly_fields = ('discount_price',)
+
+    def save_model(self, request, obj, form, change):
+        if obj.discount_active and obj.discount_percent > 0:
+            obj.discount_price = round(obj.price * (100 - obj.discount_percent) / 100, 2)
+        else:
+            obj.discount_price = None
+        super().save_model(request, obj, form, change)
+
+    def save_formset(self, request, form, formset, change):
+        instances = formset.save(commit=False)
+        for obj in instances:
+            if isinstance(obj, SubscriptionPlan):
+                if obj.discount_active and obj.discount_percent > 0:
+                    obj.discount_price = round(obj.price * (100 - obj.discount_percent) / 100, 2)
+                else:
+                    obj.discount_price = None
+            obj.save()
+        formset.save_m2m()
+
 
 @admin.register(Subscription)
 class SubscriptionAdmin(admin.ModelAdmin):
@@ -31,6 +51,8 @@ class SubscriptionAdmin(admin.ModelAdmin):
         return "-"
     short_vless_link.short_description = "VLESS"
 
+
+# Отключение мусора от django-celery-beat
 from django_celery_beat.models import (
     PeriodicTask, IntervalSchedule, CrontabSchedule,
     SolarSchedule, ClockedSchedule
